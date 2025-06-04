f4a1764b43f1cb332b69aaa2828bdbd98d381354
Angel <angeldavila0975@gmail.com>
2025-06-03 19:54:01 -0500

    feat :sparkles: add new empanada

--- main.py
+++ main.py
@@ -4,3 +4,37 @@ from funciones import *
 from funcionesjson import *
 
 menu=abrirJSON()
+booleanito = True
+
+while (booleanito):
+    menu= abrirJSON()
+    ## INICIO DEL PROGRAMA
+    print("===================================")
+    print(" Bienvenidos a el simulador Empanadas Doña Pepa")
+    print("==================================")
+    print("1.Registrar una empanada")
+    print("2.Actualizar empanada")
+    print("3.Eliminar empanada")
+    opcion= int(input("Dime una opcion numerica: "))
+    if (opcion==1):
+        nombre=input("Dime el nombre de la empanada: ")
+        precio=int(input("Dime el precio de la empanada: "))
+        cantEmpanada=int(input("Dime la cantidad de ingredientes de la empanada: "))
+        diccionarioNuevo= {
+            "id": (menu[len(menu)-1]["id"])+1,
+            "nombre": nombre,
+            "precio":precio,
+            "ingredientes":[]
+        }
+        for i in range (cantEmpanada):
+            nombreIngre=input("Dime los ingredientes: ")
+            disponibilidad=int(input("Dime si ese ingrediente esta disponible para la empanada: \n 1.si \n 2.no: \n Dime una opcion numerica: "))
+            if(disponibilidad==1):
+                disponible=("si")
+            else:
+                disponible=("no")
+            dateingredientes={
+                "nombreIngre":nombreIngre,
+                "disponibilidad":disponible
+            }
+            
